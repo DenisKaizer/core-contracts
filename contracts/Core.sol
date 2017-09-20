5098b148b50fc1f27ed85d13a3c545f5a09cbd00
dom <1kaizerd@gmail.com>
2017-09-20 11:54:32 +0300

    first commit

--- contracts/Core.sol
+++ contracts/Core.sol
@@ -2,7 +2,33 @@ pragma solidity ^0.4.0;
 
 
 contract Core {
-  function Core(){
+  address public owner = msg.sender;
 
+  mapping (address => bool) permissions;
+
+  modifier onlyOwner {
+    if (msg.sender == owner)
+    _;
   }
+
+  modifier onlyApprovedProjects{
+    assert(permission[msg.sender]);
+    _;
+  }
+
+  function setPermissions(address projectAddres) onlyOwner {
+    permissions[projectAddress] = true;
+  }
+
+  function checkPermissions(addres requesterAddres) returns(bool) {
+    return(permissions[requesterAddres]);
+  }
+
+  function getKey(address profileAddress) onlyApprovedProjects returns(address){
+    address key;
+    key = (address(sha3(block.blockhash(block.number-1))));
+    return (key);
+  }
+
 }
+
